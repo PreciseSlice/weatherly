c554e1cccaccc4a253c83ffce140419537d9074b
Spencer Herms <slherms@gmail.com>
2018-01-04 16:05:42 -0700

    adds filterData for currentWeather, refators current weather to recieve props

--- lib/SevenHour.jsx
+++ lib/SevenHour.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import {data} from './mockData';
 import HourCard from './hourCard';
 
-export default function SevenHour (props)  {
-  console.log(props.hourData);
-  return (
-    <div className="hourly-container">
-      {props.hourData.map((data, i) => {
-        return (
-          <HourCard 
-            hour={data}
-            img={data}
-            temp={data}
-            key={i}
-          />
-        )
-      })}      
-    </div>
-  )
-}
+// export default function SevenHour (props)  {
+//   //console.log(props.hourData);
+//   return (
+//     <div className="hourly-container">
+//       {props.hourData.map((data, i) => {
+//         return (
+//           <HourCard 
+//             hour={data}
+//             img={data}
+//             temp={data}
+//             key={i}
+//           />
+//         )
+//       })}      
+//     </div>
+//   )
+// }
